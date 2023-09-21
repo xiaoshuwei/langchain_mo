10d309a867300ba71cca46e7c29dee3810645e90
xiaoshuwei <18137205032@163.com>
2023-09-22 00:25:53 +0800

    add setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="langchain_mo",
+    name="langchain-mo",
     version="1.0",
     author="xiaoshuwei",
     description="MatrixOne special version !",
