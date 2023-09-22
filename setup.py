1da9b39f3e9a383cd046d27d42f745106c0e5e98
xiaoshuwei <18137205032@163.com>
2023-09-22 11:29:52 +0800

    update matrixone.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="langchain-mo",
+    name="langchain_mo",
     version="1.0",
     author="xiaoshuwei",
     description="MatrixOne special version !",
