764508872a6053355844f09c451950285188b58a
xiaoshuwei <18137205032@163.com>
2023-09-22 11:55:12 +0800

    update setup name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup ,find_packages
 
 setup(
-    name="langchainmo",
+    name="langchain_mo",
     version="1.0",
     author="xiaoshuwei",
     description="MatrixOne special version !",
